aa990d09adc721a740218c92e7f528aae267e36d
KAYHAN Solar Dev <m4likiletisim@gmail.com>
2026-05-13 14:05:12 +0300

    chore(config): allow any HTTPS hostname for next/image

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        hostname: "**",
       },
     ],
   },
